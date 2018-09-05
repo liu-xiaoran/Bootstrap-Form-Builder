6921c60d4f601dae71d9dbe4f49d30367881c38f
liu-xiaoran <mk9540@163.com>
2018-09-05 17:36:16 +0800

    尝试在requerjs外部传参数

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ define([
 ){
   return {
     initialize: function(){
-
+      var that = this;
       //Bootstrap tabs from json.
       new TabView({
         title: "Input", 
@@ -51,8 +51,7 @@ define([
       $("#formtabs li").first().addClass("active");
       // Bootstrap "My Form" with 'Form Name' snippet.
       // 定义数据
-      var collectionmodels = '[{"title":"Form Name","fields":{"id":{"label":"ID / Name","type":"input-readonly","value":"formName","name":"id"},"name":{"label":"输入名称","type":"input","value":"表单名称","name":"name"}},"fresh":true},{"title":"Text Input","fields":{"id":{"label":"ID / Name","type":"input-readonly","value":"textinput-0","name":"id"},"label":{"label":"问题","type":"textarea","value":"请输问题","name":"label"},"placeholder":{"label":"默认文字","type":"input","value":"默认文字","name":"placeholder"},"helptext":{"label":"提示文案","type":"input","value":"帮助（清空不显示）","name":"helptext"},"required":{"label":"必填","type":"checkbox","value":false,"name":"required"},"inputsize":{"label":"输入框大小","type":"select","value":[{"value":"input-mini","label":"Mini","selected":false},{"value":"input-small","label":"Small","selected":false},{"value":"input-medium","label":"Medium","selected":false},{"value":"input-large","label":"Large","selected":false},{"value":"input-xlarge","label":"Xlarge","selected":true},{"value":"input-xxlarge","label":"Xxlarge","selected":false}],"name":"inputsize"}},"fresh":false},{"title":"Text Input","fields":{"id":{"label":"ID / Name","type":"input-readonly","value":"textinput-2","name":"id"},"label":{"label":"问题","type":"textarea","value":"请输入问题","name":"label"},"placeholder":{"label":"默认文字","type":"input","value":"默认文字","name":"placeholder"},"helptext":{"label":"提示文案","type":"input","value":"帮助（清空不显示）","name":"helptext"},"required":{"label":"必填","type":"checkbox","value":false,"name":"required"},"inputsize":{"label":"输入框大小","type":"select","value":[{"value":"input-mini","label":"Mini","selected":false},{"value":"input-small","label":"Small","selected":false},{"value":"input-medium","label":"Medium","selected":false},{"value":"input-large","label":"Large","selected":false},{"value":"input-xlarge","label":"Xlarge","selected":true},{"value":"input-xxlarge","label":"Xxlarge","selected":false}],"name":"inputsize"}},"fresh":false},{"title":"Multiple Radios","fields":{"id":{"label":"ID / Name","type":"input","value":"multipleradios-0","name":"id"},"label":{"label":"Label Text","type":"input","value":"Multiple Radios","name":"label"},"required":{"label":"Required","type":"checkbox","value":false,"name":"required"},"radios":{"label":"Radios","type":"textarea-split","value":["Option one","Option two"],"name":"radios"}},"fresh":false}]';
-      var myForm = new MyFormView({
+      that.myForm = new MyFormView({
         title: "Original", 
         collection: new MyFormSnippetsCollection([
           { "title" : "Form Name", 
@@ -71,16 +70,15 @@ define([
           }
         ])
       });
-
-      $("#myClick").on("click",function(){
-        // 使用数据，重置表单
-        // myForm.initialize({},collectionmodels);
-        
-        // 输出已经存储的元素数据
-        var outData = myForm.outCollection();
-        console.log(outData);
-        
-      });
+    },
+    outFunction: function(){
+      var that = this;
+      var outData = that.myForm.outCollection();
+      return outData;
+    },
+    inFunction: function(data){
+      var that = this;
+      that.myForm.initialize({},data);
     }
   }
 });
